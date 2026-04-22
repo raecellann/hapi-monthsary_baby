bdf245c09162b67e779bfae108bd0255f819c15a
raecellann <raecellanndomingogalvez@gmail.com>
2026-04-22 18:23:23 +0800

    :rocket: initial commits

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/hapi-monthsary_baby/',
+  server: {
+    port: 3000,
+    open: true
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
